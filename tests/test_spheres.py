83a568271c5d69aa7960bbdedba81254b5c44027
Hobson Lane <github@totalgood.com>
2017-04-26 23:53:12 -0700

    closest, second and tenth

--- tests/test_spheres.py
+++ tests/test_spheres.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!usr/bin/env python3
 from __future__ import print_function, unicode_literals, division, absolute_import
 from builtins import int, round, str,  object  # noqa
 from future import standard_library
@@ -30,17 +29,22 @@ def test_sphere():
     assert 0.05 >= closest[9][-1] > 0.0003
 
 
-def test_hyperspheres(X=np.random.uniform(size=(200000, 10))):
-    """ Demonstrate curse of dimensionality and where LSH starts to fail 
+def hyperspheres_10D(X=np.random.uniform(size=(200000, 10))):
+    """ Demonstrate curse of dimensionality and where LSH starts to fail
 
     Returns:
       lsh, X, secondclosest, tenthclosest
 
+    >>> import pandas as pd
+    >>> lsh, vectors, rank1, rank2, rank10 = test_hyperspheres()
+    >>> pd.DataFrame(rank2)
+    >>> pd.DataFrame(rank10)
     """
     tenthclosest = []
     secondclosest = []
+    closest = []
     for D in range(2, X.shape[1]):
-        lsh = LSHash(int(512. / D) + 1, D, num_hashtables=D)
+        lsh = LSHash(int(1024 * 8182. / D) + D, D, num_hashtables=D)
 
         # query vector
         q = np.random.uniform(size=(D,))
@@ -52,14 +56,16 @@ def test_hyperspheres(X=np.random.uniform(size=(200000, 10))):
             x /= np.linalg.norm(x)
             distances += [1. - np.sum(x * q)]  # cosine similarity
         distances = sorted(distances)
-        closest = lsh.query(q, num_results=10, distance_func='cosine')
+        print(distances[:10])
+        closest10 = lsh.query(q, distance_func='cosine')
 
-        N = len(closest)
-        rank = min(10, N)
-        tenthclosest += [[D, N - 1, closest[rank - 1][-1] if N else None, distances[rank - 1]]]
+        N = len(closest10)
+        tenthclosest += [[D, min(9, N - 1) if N else -1, closest10[min(9, N - 1)][-1] if N else 2., distances[min(9, N - 1)]]]
+        secondclosest += [[D, min(1, N - 1) if N else -1, closest10[min(1, N - 1)][-1] if N else 2., distances[min(1, N - 1)]]]
+        closest += [[D, 0 if N else -1, closest10[0][-1] if N else 2., distances[0]]]
         print(tenthclosest[-1])
-        secondclosest += [[D, N - 1, closest[min(2, N) - 1][-1] if N else None, distances[rank - 1]]]
         print(secondclosest[-1])
-    for i, tc in enumerate(tenthclosest):
-        assert 1e-9 < tc[-2] or 1e-6 < 0.2
-    return lsh, X, secondclosest, tenthclosest
+        print(closest[-1])
+    # for i, tc in enumerate(tenthclosest):
+    #     assert 1e-9 < tc[-2] or 1e-6 < 0.2
+    return lsh, X, closest, secondclosest, tenthclosest
